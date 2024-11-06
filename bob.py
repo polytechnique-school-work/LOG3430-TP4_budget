19bd4fdfef3186b77c91575517123a06e0438d4d
Gabriel Landry <gablandry31@gmail.com>
2024-11-06 09:31:18 -0500

    Save

--- bob.py
+++ bob.py
@@ -1,4 +1,7 @@
 import os
 
-os.system("git bisect start $badhash $goodhash")
+bad_hash = os.getenv("BAD_HASH")
+good_hash = os.getenv("GOOD_HASH")
+
+os.system(f"git bisect start {bad_hash} {good_hash}")
 os.system("git bisect run python manage.py test")
